f6cecaa0d0f893a20716775e3a769fc860fd7dae
ian5142 <ian@birdisland.net>
2021-01-08 10:37:16 -0400

    Add License header, GPLv3

--- src/nema0183_aggregator/Nema0183_aggregator.java
+++ src/nema0183_aggregator/Nema0183_aggregator.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2021 Ian Van Schaick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nema0183_aggregator;
 
@@ -14,9 +25,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //import java.util.ArrayList;
-
 /**
- * This class contains the main method. It acts as a connector between the various read and single write classes.
+ * This class contains the main method. It acts as a connector between the
+ * various read and single write classes.
+ *
  * @author Ian Van Schaick
  */
 public class Nema0183_aggregator {
@@ -26,28 +38,30 @@ public class Nema0183_aggregator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayDeque <String> gpsSentences = new ArrayDeque <String> ();
-        ArrayDeque <String> aisSentences = new ArrayDeque <String> ();
-        ArrayDeque <String> headingSentences = new ArrayDeque <String> ();
-        ArrayDeque <String> sounderSentences = new ArrayDeque <String> ();
-        ArrayDeque <String> outputSentences = new ArrayDeque <String> ();
+        ArrayDeque<String> gpsSentences = new ArrayDeque<String>();
+        ArrayDeque<String> aisSentences = new ArrayDeque<String>();
+        ArrayDeque<String> headingSentences = new ArrayDeque<String>();
+        ArrayDeque<String> sounderSentences = new ArrayDeque<String>();
+        ArrayDeque<String> outputSentences = new ArrayDeque<String>();
 //        ArrayList <RS232Control> devices = new ArrayList<RS232Control> (5);
         RS232Control gps = new RS232Control("COM32", 4800, true);
         RS232Control ais = new RS232Control("COM35", 34800, false);
         RS232Control heading = new RS232Control("COM37", 4800, false);
         RS232Control sounder = new RS232Control("COM39", 4800, false);
         RS232Control output = new RS232Control("COM41", 115200, false);
-        
-        ExecutorService executor = Executors.newCachedThreadPool();
-        
+        gps.run();
+//        gps.start();
+        while (true) {
+            System.out.println(gps.getLine());
+//            gpsSentences.add(gps.getLine());
+//        ExecutorService executor = Executors.newCachedThreadPool();
 //        Runnable gpsRun = () -> {
 //            gpsSentences.add(gps.testRead2());
 //        };
 //        executor.submit(gpsRun);
-        executor.submit(() -> {
-            gpsSentences.add(gps.testRead2());
-        });
-         
+//        executor.submit(() -> {
+//            gpsSentences.add(gps.testRead2());
+//        });
 //        Runnable aisRun = () -> {
 //            aisSentences.add(ais.testRead2());
 //        };
@@ -62,21 +76,22 @@ public class Nema0183_aggregator {
 //            sounderSentences.add(sounder.testRead2());
 //        };
 //        executor.submit(sounderRun);
-//        try {
-//            Thread.sleep(10 * 1000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(Nema0183_aggregator.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        executor.shutdown();
         try {
-            executor.awaitTermination(1 * 10000, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Nema0183_aggregator.class.getName()).log(Level.SEVERE, null, ex);
         }
+//        executor.shutdown();
+//        try {
+//            executor.awaitTermination(1 * 10000, TimeUnit.MILLISECONDS);
+//        } catch (InterruptedException e) {
+//        }
 //        for (int i = 0 ; i < 5 ; i++) {
-        if (!(gpsSentences.isEmpty()) ) {
-            String line = gpsSentences.removeFirst();
-            System.out.println(line);
-            output.testWrite(line);
+//            if (!(gpsSentences.isEmpty())) {
+//                String line = gpsSentences.removeFirst();
+//                System.out.println(line);
+//                output.testWrite(line);
+//            }
         }
 //        try {
 //            Thread.sleep(2 * 1000);
@@ -189,7 +204,7 @@ public class Nema0183_aggregator {
 //                Logger.getLogger(Nema0183_aggregator.class.getName()).log(Level.SEVERE, null, ex);
 //            }
 //        ArrayList<String> portList = gps.findPort();
-        
+
 //        System.out.println("Ports: ");
 //        portList.forEach(str -> {
 //            System.out.println(str);
